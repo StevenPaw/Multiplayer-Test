8ca19b2cb1487f3259879e01f88080ad1baa50e7
Steffen Kahl <steffen.kahl@ue-germany.de>
2021-03-02 11:33:00 +0100

    [ADDED] Player and Multiplayer Hosting/Joining

---
+++ Assets/_OwnAssets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
+            transform.position = transform.position + movement;
+        }
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+    }
+}
