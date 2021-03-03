606d40a7d14bb0522c1ad95759e78c23f5363799
Steffen Kahl <steffen.kahl@ue-germany.de>
2021-03-03 02:38:30 +0100

    [ADDED] 3D-Map

---
+++ Assets/_OwnAssets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class CameraController : NetworkBehaviour
+{
+    [SerializeField]
+    private float offsetX = 0;
+    [SerializeField]
+    private float offsetY = 0;
+    [SerializeField]
+    private float offsetZ = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            Camera.main.transform.position =
+                this.transform.position - new Vector3(offsetX,offsetY,offsetZ);
+            Camera.main.transform.LookAt(this.transform.position);
+            Camera.main.transform.parent = this.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/_OwnAssets/Scripts/Player.cs
+++ Assets/_OwnAssets/Scripts/Player.cs
@@ -16,7 +16,7 @@ public class Player : NetworkBehaviour
         {
             float moveHorizontal = Input.GetAxis("Horizontal");
             float moveVertical = Input.GetAxis("Vertical");
-            Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
+            Vector3 movement = new Vector3(moveHorizontal * 0.1f, 0, moveVertical * 0.1f);
             transform.position = transform.position + movement;
         }
     }
