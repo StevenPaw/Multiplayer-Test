dd74dc1e9bce535b2ecbf483545b9793deaabc5c
Steffen Kahl <steffen.kahl@ue-germany.de>
2021-03-10 22:40:36 +0100

    [ADDED] Tab Navigation

---
+++ Assets/_OwnAssets/Scripts/InputTabNavigation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InputTabNavigation : MonoBehaviour
+{
+    EventSystem system;
+    void Start ()
+    {
+        system = EventSystem.current;
+    }
+    
+    public void Update()
+    {
+ 
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+ 
+            if (next!= null) {
+                       
+                InputField inputfield = next.GetComponent<InputField>();
+                if (inputfield !=null) inputfield.OnPointerClick(new PointerEventData(system));  //if it's an input field, also set the text caret
+                       
+                system.SetSelectedGameObject(next.gameObject, new BaseEventData(system));
+            }
+            //else Debug.Log("next nagivation element not found");
+   
+        }
+    }
+
+}
